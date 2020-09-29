135af731f0d1e97515b5190c4ad07707c34034ad
Noman Kazi <60215267+iamnomankazi@users.noreply.github.com>
2020-09-29 22:53:20 +0530

    Update Coffee_Chatbot.py

--- Coffee_Chatbot.py
+++ Coffee_Chatbot.py
@@ -1,7 +1,7 @@
 # You’re given the task of creating a Python chatbot that 
 # can help cut the wait time of a normal coffee run by taking customers’ orders in advance.
 
-# -------- On the way Cafe's Chatbot to order coffee -----------
+# -------- Cafe's Chatbot to order coffee online -----------
 
 # Define your functions
 
